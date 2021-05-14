b18574eafe559812107ed9fc54723b2a9f8560f0
Troy <troyrnoble98@gmail.com>
2021-05-14 13:38:16 -0400

    updates on main branch

--- numguesser.py
+++ numguesser.py
@@ -2,10 +2,11 @@ import random
 
 random.seed()
 
-def checkNum(usernum, numtype):
+
+def checkNum(usernum, message):
     while True:
         try:
-                usernum = int(input("Please enter your " + numtype + " non-negative number: "))
+                usernum = int(input(message))
         except ValueError:
                 print("Thats not an integer! Please try again.")
                 continue
@@ -18,6 +19,7 @@ def checkNum(usernum, numtype):
                 break
     return usernum
 
+
 def main ():
     lownum = 0
     highnum = 0
@@ -27,9 +29,17 @@ def main ():
     print("Well hello there, ", name, ", we're happy to have you!")
     print("Here, you'll guess between two numbers.")
 
-    lownum = checkNum(lownum, "low")
-    highnum = checkNum(highnum, "high")
+    lownum = checkNum(lownum, "Please enter your low, non-negative integer: ")
+    highnum = checkNum(highnum, "Please enter your high, non-negative integer: ")
+    score = checkNum(score,"Please enter your starting score: ")
 
+    # while True:
+    #     if (score <= 0):
+    #         print("No more points! Come back again.")
+    #         break
+    #     randomnumber = random.randint(lownum,highnum)
+
+        
         
 
 
